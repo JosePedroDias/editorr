3ab44aec3df1bcf7150c11b95a02fe273875e7b1
José Pedro Dias <jose.pedro.dias@gmail.com>
2014-02-14 11:34:43 +0000

    minor changes

---
+++ README.md
@@ -0,0 +1,5 @@
+# E D I T O R R
+
+For now an attempt to do a text editor with custom parser.
+
+The idea is to recreate hemingwayapp's features for the portuguese language, we'll see...
--- ajax.js
+++ ajax.js
@@ -1,27 +1,27 @@
 'use strict';
 
 var ajax = function(uri, cb) {
-	if (!cb) { cb = function() {}; }
-	var xhr = new XMLHttpRequest();
-	xhr.withCredentials = true;
-	xhr.open('POST', uri, true);
-	var cbInner = function() {
-		if (xhr.readyState === 4 && xhr.status === 200) {
-			return cb(null, JSON.parse(xhr.response));
-		}
-		cb('error requesting ' + uri);
-	};
-	xhr.onload  = cbInner;
-	xhr.onerror = cbInner;
-	xhr.send(null);
+    if (!cb) { cb = function() {}; }
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    xhr.open('POST', uri, true);
+    var cbInner = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            return cb(null, JSON.parse(xhr.response));
+        }
+        cb('error requesting ' + uri);
+    };
+    xhr.onload  = cbInner;
+    xhr.onerror = cbInner;
+    xhr.send(null);
 };
 
 
 
 var ajaxSync = function(uri) {
-	var xhr = new XMLHttpRequest();
-	xhr.withCredentials = true;
-	xhr.open('GET', uri, false);
-	xhr.send(null);
-	return (xhr.status === 200)  ? xhr.responseText : undefined;
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    xhr.open('GET', uri, false);
+    xhr.send(null);
+    return (xhr.status === 200)  ? xhr.responseText : undefined;
 };
